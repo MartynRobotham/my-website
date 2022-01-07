8ed54b18bc40bdc185016ab7008659be1d11ea9a
MartynRobotham <martyn_robotham@hotmail.co.uk>
2022-01-07 13:36:16 +0000

    add favicon and start about section content

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,12 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://martynrobotham.co.uk",
     title: "personal-website",
+    icon: "src/images/favicon/favicon.ico"
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: ["gatsby-plugin-sass", {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      icon: "src/images/favicon/icon.png"
+    }
+  }],
 };
--- src/pages/sub-pages/about.js
+++ src/pages/sub-pages/about.js
@@ -3,7 +3,16 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            About us section
+            <h2>About me</h2>
+            <p>I am an experienced application/Web Developer, specialising in .NET core and full stack development since
+                2015, with a strong focus on React, Angular and Progressive Web Applications (PWA). I have helped build
+                customer portals and customer facing platforms to help solve problems and help the business achieve
+                their goals. I also have experience using SQL, Mongo and DynamoDB to store data when building web
+                applications.</p>
+            <p>My focus currently is to continue learning and improve as a developer, using technologies such as React &
+                C#, developing new features and implementing bug fixes. I am an approachable, polite hardworking,
+                committed individual with a passion to grow as a programmer and an individual with my skills, knowledge
+                and expertise continuing to improve, who is willing to help others.</p>
         </div>
     );
 };
