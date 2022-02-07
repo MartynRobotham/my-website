bba4b4fbc17bd461a647faa42f9357f3d0e6a334
MartynRobotham <martyn_robotham@hotmail.co.uk>
2022-02-07 16:59:33 +0000

    continue with tweaks to new website

--- src/pages/sub-pages/portfolio.js
+++ src/pages/sub-pages/portfolio.js
@@ -6,15 +6,19 @@ const Portfolio = () => {
         <React.Fragment>
             <div className="portfolio-grid">
                 <div className="one">
-                    <p>This website</p>
-                    <p>React, Gatsby, PWA</p></div>
+                    <h4>This website</h4>
+                    <p>I have created this website using a range of technologies, primarily as a Gastsby PWA</p>
+                    <a href='https://www.gatsbyjs.com/'>Interested? Click here to visit</a>
+                </div>
                 <div className="two">
-                    <p>Blood website</p>
-                    <p>Angular, Bing maps, PWA</p>
+                    <h4>Blood donation project</h4>
+                    <p>A side project that gives a detailed history of my blood donations</p>
+                    <a href='https://blood-ibnhsa.martynrobotham.co.uk/'>Interested? Click here to visit</a>
                 </div>
                 <div className="three">
-                    <p>Open Source contributions</p>
-                    <p>JS, Documentation</p>
+                    <h4>Open Source contributions</h4>
+                    <p>I have made small contributions to open source projects, including single-spa.js</p>
+                    <a href='https://github.com/single-spa/single-spa.js.org/graphs/contributors'>Interested? Click here to visit</a>
                 </div>
             </div>
         </React.Fragment>
