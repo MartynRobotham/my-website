81349e9ed2119d23b11b4d34375508ac66c70a23
MartynRobotham <martyn_robotham@hotmail.co.uk>
2022-03-23 15:12:25 +0000

    feat: implement correct job dates and fix styling on carousel

--- src/pages/sub-pages/carousel/carousel.js
+++ src/pages/sub-pages/carousel/carousel.js
@@ -25,31 +25,31 @@ const jobHistory = [
     {
         position: 'Full Stack Developer',
         company: 'Gnatta',
-        duration: 'May 2021 - Present',
+        duration: 'October 2020 - May 2021',
         description: 'Using .NET Core Web API, React & MongoDB, I have been involved in the development of Gnatta\'s communication software platform. This has included building and optimising features and fixing bugs across the stack, following a CI/CD process, using microservices and dockerised containers to help maintain a large communication platform at scale. I have also developed hosted services to backfill old data to use new database structures.'
     },
     {
         position: 'Software Developer',
         company: 'Orisec',
-        duration: 'May 2021 - Present',
+        duration: 'February 2020 - July 2020',
         description: 'Using .NET Core & React, I am involved with the development of their portal application to allow users to manage their security devices. I am working with a .NET Core API that follows the repository pattern which returns JSON data to display via. the React front end application. I have developed new features for the customer portal such as an enhanced user management tool and an advanced monitoring tool for control panels.'
     },
     {
         position: 'Software Developer',
         company: 'RightIndem',
-        duration: 'May 2021 - Present',
+        duration: 'November 2018 - November 2019',
         description: 'Using React & .NET techologies, I am involved with developing new functionality and updating existing functionality within their insurance platforms. This involves me working on CQRS & event sourcing in .NET applications that use C# to transport messages to the react front end application by creating new messages or updating the existing messages that are sent to the front end application. On the react application, I am tasked with helping to create new pages within the application journey or updating existing pages to resolve bugs or implement new functionality.'
     },
     {
         position: 'Web Developer',
         company: 'See Tickets',
-        duration: 'May 2021 - Present',
+        duration: 'June 2018 - November 2018',
         description: 'Using Angular and .NET, I am developing new features and fixing bugs in the client console application where companies can log in, advertise and manage their shows through their online platform. My role also includes resolving issues within the SQL database using Postgres. I am using Visual Studio with TFS to develop and track my completed work.'
     },
     {
         position: 'Web Applications Developer',
         company: 'Utilitywise',
-        duration: 'May 2021 - Present',
+        duration: 'June 2015 - May 2018',
         description: 'Using Angular with Typescript, I have helped developed their price comparison website and customer portal applications, implementing new features and fixing bugs within these applications. I have also developed API\'s using node.js and C# to allow communication between these applications and external applications too using internal and external storage solutions including SQL, MongoDB and Azure. GIT, Webstorm and JIRA are used to develop the applications, track the projects and use version control.'
     },
 ];
@@ -114,14 +114,13 @@ function Carousel() {
             </AutoPlaySwipeableViews>
             {/*TODO: Override styling as will eventually remove mat UI and make no additional plugin*/}
             <MobileStepper
-                style={{backgroundColor: 'transparent', color: '#05386b'}}
+                style={{backgroundColor: 'transparent'}}
                 steps={maxSteps}
                 position="static"
                 activeStep={activeStep}
                 nextButton={
                     <React.Fragment>
                         <Button
-                            color={'#05386b'}
                             size="small"
                             onClick={handleNext}
                             disabled={activeStep === maxSteps - 1}
@@ -133,14 +132,15 @@ function Carousel() {
                             )}
                         </Button>
                         <Button
+                            style={{color: '#05386b'}}
                             onClick={changeCarouselState}>
                             {/*// TODO: Change MUI Icons to font awesome free icons -> try to remove as much MUI and have vanilla as possible*/}
-                            <Typography color={'#05386b'}>{intervalSize !== pauseDuration ? <PauseIcon/> : <PlayArrowIcon/>}</Typography>
+                            <Typography style={{color: '#05386b'}}>{intervalSize !== pauseDuration ? <PauseIcon/> : <PlayArrowIcon/>}</Typography>
                         </Button>
                     </React.Fragment>
                 }
                 backButton={
-                    <Button color={'#05386b'} size="small" onClick={handleBack} disabled={activeStep === 0}>
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
                         {theme.direction === 'rtl' ? (
                             <KeyboardArrowRight/>
                         ) : (
