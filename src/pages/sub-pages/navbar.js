1030703618c60d375231aa4695f04883f0477e5b
MartynRobotham <martyn_robotham@hotmail.co.uk>
2022-09-23 15:44:38 +0100

    fix navigation bar issues on website

--- src/pages/sub-pages/navbar.js
+++ src/pages/sub-pages/navbar.js
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+import {Link, MenuItem, Button, Container, IconButton, Toolbar, Box, AppBar} from "@mui/material";
+import {useTheme} from "@mui/material/styles";
 
-const pages = ['Home', 'About', 'Work', 'Portfolio', 'Contact'];
+const pages = ['Home', 'About', 'Experience', 'Portfolio', 'Contact'];
 
 const Navbar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const theme = useTheme();
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -27,7 +22,7 @@ const Navbar = () => {
         <AppBar position="static" className={'material-header'} color={'default'}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters variant={'dense'}>
-                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}, paddingTop: '1.75vh'}}>
                         <IconButton
                             size="large"
                             aria-label="navigation menu bar"
@@ -60,15 +55,16 @@ const Navbar = () => {
                             <Box>
                                 {pages.map((page) => (
                                     <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center">{page}</Typography>
+                                        <Link style={{color: theme.palette.info.main, textDecoration: 'none'}} href={`#${page.toLowerCase()}`} textAlign="center">{page}</Link>
                                     </MenuItem>
                                 ))}
                             </Box>
                         </Menu>
                     </Box>
-                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}, justifyContent: 'space-between'}}>
                         {pages.map((page) => (
                             <Button
+                                href={`#${page.toLowerCase()}`}
                                 key={page}
                                 onClick={handleCloseNavMenu}
                                 sx={{my: 2, color: '#3A4249', display: 'block'}}
