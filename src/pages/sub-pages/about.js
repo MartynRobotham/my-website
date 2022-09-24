9981284bfc9844ec3dd9f5600964327e221e3067
MartynRobotham <martyn_robotham@hotmail.co.uk>
2022-09-24 16:33:14 +0100

    Update about me section and reduce excess padding

--- src/pages/sub-pages/about.js
+++ src/pages/sub-pages/about.js
@@ -4,15 +4,15 @@ const About = () => {
     return (
         <div>
             <h2>About me</h2>
-            <p>I am an experienced application/Web Developer, specialising in .NET core and full stack development since
-                2015, with a strong focus on React, Angular and Progressive Web Applications (PWA). I have helped build
-                customer portals and customer facing platforms to help solve problems and help the business achieve
-                their goals. I also have experience using SQL, Mongo and DynamoDB to store data when building web
-                applications.</p>
-            <p>My focus currently is to continue learning and improve as a developer, using technologies such as React &
-                C#, developing new features and implementing bug fixes. I am an approachable, polite hardworking,
-                committed individual with a passion to grow as a programmer and an individual with my skills, knowledge
-                and expertise continuing to improve, who is willing to help others.</p>
+            <p>I am an experienced application/Web Developer, specialising in front end development but has also
+                developed full stack using .NET frameworks since 2015, with a strong focus on React, Angular and
+                Progressive Web Applications (PWA). I have helped build customer portals and customer facing platforms
+                to help solve problems and help the business achieve their goals. I also have experience using SQL,
+                Mongo and DynamoDB to store data when building web applications.</p>
+            <p>My focus currently is to continue learning and improve as a developer, using technologies such as React,
+                GraphQL & Node.js, through developing new features and implementing bug fixes. I am an approachable,
+                polite hardworking, committed individual with a passion to grow as a programmer and an individual with
+                my skills, knowledge and expertise while continuing to improve myself and others.</p>
         </div>
     );
 };
