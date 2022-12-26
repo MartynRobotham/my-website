8d1ad9d76a0806641f084faf819d245b35cb1541
MartynRobotham <martyn_robotham@hotmail.co.uk>
2022-12-26 22:01:20 +0000

    implement vanta rings animation

--- src/pages/sub-pages/header/header.js
+++ src/pages/sub-pages/header/header.js
@@ -1,39 +1,74 @@
-import React from 'react';
 import "./header.scss";
+import React, {useState, useEffect, useRef} from 'react'
+import RINGS from 'vanta/dist/vanta.rings.min'
+import * as THREE from "three";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGithub, faLinkedin, faStackOverflow} from '@fortawesome/free-brands-svg-icons';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 
 const Header = () => {
+
+    const [vantaEffect, setVantaEffect] = useState(0);
+    const vantaRef = useRef(null);
+
+    useEffect(() => {
+        if (!vantaEffect) {
+            setVantaEffect(
+                RINGS({
+                    el: vantaRef.current,
+                    THREE: THREE,
+                    mouseControls: true,
+                    touchControls: true,
+                    gyroControls: false,
+                    minHeight: 600.0,
+                    minWidth: 600.0,
+                    scale: 1.0,
+                    scaleMobile: 1.0,
+                    backgroundColor: 0xabf62d
+                    // color1: "#0000ff",
+                    // color2: "#ffffff"
+                })
+            );
+        }
+        return () => {
+            if (vantaEffect) vantaEffect.destroy();
+        };
+    }, [vantaEffect]);
+
     return (
-        <div className='test'>
-            <h1>Martyn Robotham</h1>
-            <h2>UI Developer with full stack experience</h2>
-            <div className={'icon-grid'}>
-                <p className={'start-item'}>
-                    <a href={'mailto:martyn_robotham@hotmail.co.uk'}>
-                        <FontAwesomeIcon icon={faEnvelope} title={'Email'}/>
-                    </a>
-                </p>
-                <p className={'end-item'}>
-                    <a href={'https://github.com/MartynRobotham'}>
-                        <FontAwesomeIcon icon={faGithub} title={'GitHub'}/>
-                    </a>
-                </p>
-                <p className={'start-item'}>
-                    <a href={'https://www.linkedin.com/in/martyn-robotham-21617a87/'}>
-                        <FontAwesomeIcon icon={faLinkedin}
-                                         title={'LinkedIn'}/>
-                    </a>
-                </p>
-                <p className={'end-item'}>
-                    <a href={'https://stackoverflow.com/users/6200847/martyn93'}>
-                        <FontAwesomeIcon icon={faStackOverflow}
-                                         title={'Stack Overflow'}/>
-                    </a>
-                </p>
+        <React.Fragment>
+            <div ref={vantaRef} className={'temp'}>
+                <div className='test'>
+                    <h1>Martyn Robotham</h1>
+                    <h2>UI Developer with full stack experience</h2>
+                    <div className={'icon-grid'}>
+                        <p className={'start-item'}>
+                            <a href={'mailto:martyn_robotham@hotmail.co.uk'}>
+                                <FontAwesomeIcon icon={faEnvelope} title={'Email'}/>
+                            </a>
+                        </p>
+                        <p className={'end-item'}>
+                            <a href={'https://github.com/MartynRobotham'}>
+                                <FontAwesomeIcon icon={faGithub} title={'GitHub'}/>
+                            </a>
+                        </p>
+                        <p className={'start-item'}>
+                            <a href={'https://www.linkedin.com/in/martyn-robotham-21617a87/'}>
+                                <FontAwesomeIcon icon={faLinkedin}
+                                                 title={'LinkedIn'}/>
+                            </a>
+                        </p>
+                        <p className={'end-item'}>
+                            <a href={'https://stackoverflow.com/users/6200847/martyn93'}>
+                                <FontAwesomeIcon icon={faStackOverflow}
+                                                 title={'Stack Overflow'}/>
+                            </a>
+                        </p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </React.Fragment>
+
     );
 };
 
