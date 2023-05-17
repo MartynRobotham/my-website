58a9613497ad8511e9ce0b25186f9f9822ed5599
MartynRobotham <martyn_robotham@hotmail.co.uk>
2023-05-17 14:08:05 +0100

    Update about me text

--- src/pages/sub-pages/about.js
+++ src/pages/sub-pages/about.js
@@ -7,32 +7,21 @@ const About = () => {
             <div className={'about-me-profile'}>
                 <img className={'extend-image'} width={'40%'} src={'/images/headshot.jpg'} alt={'my headshot'}/>
                 <div>
-                    <p className={'alternate-text'}>I am an experienced Application / Web Developer specialising in
-                        front
-                        end development, but I also have
-                        the skill set to work full stack using the .NET frameworks since 2015 - with a strong focus on
-                        React,
-                        Angular and Progressive Web Applications (PWA). I have helped build customer portals and
-                        customer
-                        facing
-                        platforms to solve problems and help the business achieve their goals. Furthermore, I carry
-                        experience
-                        using SQL, Mongo, GraphQL and DynamoDB to store and retrieve data when building web
-                        applications.</p>
-                    <p className={'alternate-text'}>My current goals are to move into a senior developer role and
-                        continue
-                        to learn and improve as a
-                        developer, using technologies such as React, GraphQL & Node.js, through developing new features
-                        and
-                        implementing bug fixes. I am known to be an approachable, polite, hard-working, committed
-                        individual
-                        with a passion to grow as a programmer and as an individual with my skill set, expertise and
-                        knowledge,
-                        I am always looking for ways to improve, and I am always ready to help others achieve their long
-                        term
-                        goals.</p>
+                    <p className={'alternate-text'}>As an experienced Application/Web Developer, I specialize in
+                        front-end development with a skill set that extends to full-stack development using .NET
+                        frameworks since 2015. My expertise lies in React, Angular, and Progressive Web Applications
+                        (PWA). I have a track record of building customer portals and customer-facing platforms to solve
+                        problems and help businesses achieve their goals. I am proficient in using SQL, Mongo, GraphQL,
+                        and DynamoDB to store and retrieve data when building web applications.</p>
+                    <p className={'alternate-text'}>My goal is to take on a senior developer role while continuing to
+                        learn and improve as a developer. I aim to utilize technologies like React, GraphQL, and Node.js
+                        in developing new features and implementing bug fixes. I am approachable, polite, hard-working,
+                        and committed, with a passion to grow as a programmer and an individual with my expertise,
+                        knowledge, and skill set. I strive to continuously improve and help others achieve their
+                        long-term goals.</p>
                     <div className={'tech-assets'}>
-                        <h3>I have multiple years experience using these technologies listed below and many more</h3>
+                        <h3>Over the years, I have honed my expertise in various technologies, including but not limited
+                            to those listed below.</h3>
                         <div className={'logo-grid'}>
                             <div style={{gridColumn: 1}}>
                                 <img src={'/images/logos/javascript.svg'} height={'100px'} alt={'JavaScript'}/>
