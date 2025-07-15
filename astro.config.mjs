1c73d575b708909c2ccc43f4e1c1a9bc446d2163
Martyn Robotham <martyn_robotham@hotmail.co.uk>
2025-07-15 20:44:42 +0100

    implement basic config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MartynRobotham.github.io/my-website/', // Important for correct routing
+  base: '/my-website/', // Needed for GitHub Pages
+  outDir: './dist',
   integrations: [react()]
 });
