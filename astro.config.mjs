73b2aed55cd837f413c5c9e353469227f646949c
Martyn Robotham <martyn_robotham@hotmail.co.uk>
2026-09-13 22:14:02 +0100

    Comment out PWA in manifest to fix in later build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import AstroPWA from '@vite-pwa/astro';
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -8,33 +7,33 @@ export default defineConfig({
   site: 'https://martynrobotham.co.uk/',
   outDir: './dist',
   integrations: [
-      react(),
-    AstroPWA({
-      registerType: 'autoUpdate', // or your preference
-      includeAssets: [
-        'assets/favicon/android-chrome-192x192.png',
-        'assets/favicon/android-chrome-512x512.png',
-        'assets/favicon/favicon.ico',
-      ],
-      manifest: {
-        name: 'My App',
-        short_name: 'App',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#2A2A2A',
-        theme_color: '#2A2A2A',
-        icons: [
-          {
-            src: 'assets/favicon/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/favicon/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    })]
+      react(),]
+    // AstroPWA({
+    //   registerType: 'autoUpdate', // or your preference
+    //   includeAssets: [
+    //     'assets/favicon/android-chrome-192x192.png',
+    //     'assets/favicon/android-chrome-512x512.png',
+    //     'assets/favicon/favicon.ico',
+    //   ],
+    //   manifest: {
+    //     name: 'My App',
+    //     short_name: 'App',
+    //     start_url: '/',
+    //     display: 'standalone',
+    //     background_color: '#2A2A2A',
+    //     theme_color: '#2A2A2A',
+    //     icons: [
+    //       {
+    //         src: 'assets/favicon/android-chrome-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'assets/favicon/android-chrome-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //   },
+    // })]
 });
